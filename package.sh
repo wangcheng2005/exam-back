cf5f54e0f4668c02d36ea02c2ea43d7595baf3e5
cw85804439 <cw85804439@gmail.com>
2025-11-25 22:41:31 +0800

    init

--- package.sh
+++ package.sh
@@ -6,17 +6,17 @@ pnpm run build:prod
 
 # 复制打包好的文件到远程服务器
 echo "复制文件到远程服务器..."
-ssh  -i ./ssh.pem root@106.55.158.26 << EOF
+ssh  -i ./ssh.pem root@8.140.51.188 << EOF
   cd /data/web
   rm -rf ./dist-prod
 EOF
-scp  -i ./ssh.pem -r ./dist-prod root@106.55.158.26:/data/web
+scp  -i ./ssh.pem -r ./dist-prod root@8.140.51.188:/data/web
 
 # 远程执行脚本
 echo "远程执行脚本..."
-ssh  -i ./ssh.pem root@106.55.158.26 << EOF
+ssh  -i ./ssh.pem root@8.140.51.188 << EOF
   cd /data
   # 使用 Docker Compose 运行程序
   echo "使用 Docker Compose 运行程序..."
-  docker-compose up -d --force-recreate nginx
+  docker compose up -d --force-recreate nginx
 EOF
